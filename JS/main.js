d265cdd4107b11812bc9e0fa7601cbda922f80a6
Wilber30 <wilbersears@gmail.com>
2021-11-07 15:40:20 +0000

    - Corrected typewriter effect.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    strings: ['My Name is William Sears'],
-    autoStart: true
+  loop: false,
 });
+
+typewriter.typeString('My Name is William Sears')
+    .start();
+
+// click on menu at small breakpoints
+// set display of of top_navigation to none
+// An event listen on menu button causes top_navigation display 'block'
+
+const topNav = document.getElementsByClassName('top_navigation').style.display = "block";
