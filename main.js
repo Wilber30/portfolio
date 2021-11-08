9997e7addfa9c36939a4ee6dda6356659f645660
Wilber30 <wilbersears@gmail.com>
2021-11-08 17:27:06 +0000

    - Added form validation.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+// Top navigation reveal and hide
 
 let nav = document.getElementById("top_navigation");
 let btn = document.getElementById("reveal");
@@ -8,15 +8,38 @@ let close = document.getElementById("close");
 btn.addEventListener('click', () => {
     nav.style.display = 'block';
     close.style.display = 'block';
+    document.getElementById('page').style.marginTop = "140px";
   })
 
 close.addEventListener('click', () => {
     nav.style.display = 'none';
     close.style.display = 'none';
+    document.getElementById('page').style.marginTop = "0px";
   })
+
+// Typewriter jQuery
 
   var app = document.getElementById('app');
 
-    var typewriter = new Typewriter(app, {loop: false,});
+  var typewriter = new Typewriter(app, {loop: false,});
 
-    typewriter.typeString('My Name is William Sears').start();
+  typewriter.typeString('My Name is William Sears').start();
+
+// Email Validation
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const error = document.getElementById("error")
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null){
+    messages.push('Name is required')
+  }
+
+  if (messages.length > 0) {
+      e.preventDefault()
+      error.innerText = messages.join(', ')
+  }
+})
