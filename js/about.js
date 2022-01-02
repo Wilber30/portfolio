1cc75d808b2a0a81e0c6d06bc6032f3bb9165302
Wilber30 <wilbersears@gmail.com>
2022-01-02 11:51:03 +0000

    - Fixes about_page #content and slide-out menu.

--- js/about.js
+++ js/about.js
@@ -3,15 +3,14 @@ const nav = document.getElementById("top_navigation");
 const btn = document.getElementById("reveal");
 const close = document.getElementById("close");
 const cross = document.getElementById("cross");
-const codeMain = document.getElementById("code_main");
-
+const aboutPage = document.getElementById("content");
 
 // Arrow alignment containers from site pages
 const arrowAlign = document.getElementById("arrow_alignment");
 
 // On click this changes the top nav's display value
 btn.addEventListener('click', () => {
-  codeMain.style.marginTop = "0px";
+  aboutPage.style.marginTop = "0px";
   arrowAlign.classList.toggle('open');
   nav.classList.toggle('is-active');
   btn.classList.toggle('hide');
@@ -19,7 +18,7 @@ btn.addEventListener('click', () => {
 })
 
 close.addEventListener('click', () => {
-  codeMain.style.marginTop = "-148px";
+  aboutPage.style.marginTop = "-148px";
   arrowAlign.classList.remove('open');
   nav.classList.toggle('is-active');
   close.classList.remove('appears');
@@ -27,7 +26,7 @@ close.addEventListener('click', () => {
 })
 
 cross.addEventListener('click', () => {
-  codeMain.style.marginTop = "-148px";
+  aboutPage.style.marginTop = "-148px";
   arrowAlign.classList.remove('open');
   nav.classList.remove('is-active');
   close.classList.remove('appears');
