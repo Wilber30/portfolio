1812281e5bbbe017e05d3d68aabd18cead46f882
Wilber30 <wilbersears@gmail.com>
2022-01-02 13:31:12 +0000

    - Adds scs.js/ corrects the position value of arrow alignment.

---
+++ js/scs.js
@@ -0,0 +1,34 @@
+// Top navigation show and hide
+const nav = document.getElementById("top_navigation");
+const btn = document.getElementById("reveal");
+const close = document.getElementById("close");
+const cross = document.getElementById("cross");
+const scsPage = document.getElementById("page");
+
+// Arrow alignment containers from site pages
+const arrowAlign = document.getElementById("scs_arrow_align");
+
+// On click this changes the top nav's display value
+btn.addEventListener('click', () => {
+  scsPage.style.marginTop = "0px";
+  arrowAlign.classList.toggle('open');
+  nav.classList.toggle('is-active');
+  btn.classList.toggle('hide');
+  close.classList.toggle('appears');
+})
+
+close.addEventListener('click', () => {
+  scsPage.style.marginTop = "-148px";
+  arrowAlign.classList.remove('open');
+  nav.classList.toggle('is-active');
+  close.classList.remove('appears');
+  btn.classList.remove('hide');
+})
+
+cross.addEventListener('click', () => {
+  scsPage.style.marginTop = "-148px";
+  arrowAlign.classList.remove('open');
+  nav.classList.remove('is-active');
+  close.classList.remove('appears');
+  btn.classList.remove('hide');
+})
