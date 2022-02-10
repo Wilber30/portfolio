cfd46c271ded5e28e84f3d190c600d594c0ec7b4
Wilber30 <wilbersears@gmail.com>
2022-02-10 16:13:51 +0000

    - Changes database credentials for MyPHPAdmin.

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbServername = "127.0.0.1";
-$dbUsername = "will";
-$dbPassword = "maine";
-$dbName = "portfolio";
+$dbServername = "localhost";
+$dbUsername = "williams_sears";
+$dbPassword = "6O6nNLI8p2DwLeQI1m7t";
+$dbName = "williams_portfolio";
 
 // test
-  $conn = mysqli_connect('127.0.0.1', 'will', 'maine', 'portfolio');
+  $conn = mysqli_connect('localhost', 'williams_sears', '6O6nNLI8p2DwLeQI1m7t', 'williams_portfolio');
   // Check connection
     if($conn === false) {
       die("ERROR: Could not connect. " . mysqli_connect_error());
