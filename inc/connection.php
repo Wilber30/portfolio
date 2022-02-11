12b0283ed7652f02f8f4b9f4e639f5fa91b307f0
Wilber30 <wilbersears@gmail.com>
2022-02-11 14:46:52 +0000

    - Changes connection credentials.

--- inc/connection.php
+++ inc/connection.php
@@ -1,20 +1,20 @@
 <?php
 
-// $dbServername = "localhost";
-// $dbUsername = "williams_sears";
-// $dbPassword = "6O6nNLI8p2DwLeQI1m7t";
-// $dbName = "williams_portfolio";
+$dbServername = "localhost";
+$dbUsername = "williams_sears";
+$dbPassword = "6O6nNLI8p2DwLeQI1m7t";
+$dbName = "williams_portfolio";
 
-$dbServername = "127.0.0.1";
-$dbUsername = "will";
-$dbPassword = "maine";
-$dbName = "portfolio";
+// $dbServername = "127.0.0.1";
+// $dbUsername = "will";
+// $dbPassword = "maine";
+// $dbName = "portfolio";
 
 
 // test
-$conn = mysqli_connect('127.0.0.1', 'will', 'maine', 'portfolio');
+// $conn = mysqli_connect('127.0.0.1', 'will', 'maine', 'portfolio');
+$conn = mysqli_connect('localhost', 'williams_sears', '6O6nNLI8p2DwLeQI1m7t', 'williams_portfolio');
 
-  // $conn = mysqli_connect('localhost', 'williams_sears', '6O6nNLI8p2DwLeQI1m7t', 'williams_portfolio');
   // Check connection
     if($conn === false) {
       die("ERROR: Could not connect. " . mysqli_connect_error());
