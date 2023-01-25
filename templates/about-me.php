e275e94554c8846554d6dc6db691ddde13b5aa83
Wilber30 <wilbersears@gmail.com>
2023-01-25 13:16:24 +0000

    Moved to template folder

---
+++ templates/about-me.php
@@ -0,0 +1,89 @@
+<?php include 'includes/head.php' ?>
+
+<?php include 'includes/topnav.php' ?>
+
+<?php include 'includes/sidenav.php' ?>
+  <main>
+    <div id="content">
+      <div class="overlay">
+        <div class="vessel">
+          <div id="arrow_alignment">
+            <!-- <div id="close"><i class="fas fa-arrow-circle-up home_close"></i></div>
+            <div id="reveal"><i class="fas fa-arrow-circle-down home_reveal"></i></div> -->
+
+            <div id="close"><i class="fas fa-arrow-circle-up"></i></div>
+            <div id="reveal"><i class="fas fa-arrow-circle-down"></i></div>
+          </div>
+          <div class="section">
+            <div id="about_wrapper">
+              <div class="text">
+
+                <div class="biograph_wrapper">
+                  <h1 class="about_heading">About me</h1>
+
+                <p id="biograph">
+                  My name is William Sears, I am a web developer based in Oxfordshire, UK.
+                  I am currently studying on a Web Development course run by Netmatters;
+                  the course teaches the fundamental skills required for a career in Web Development
+                  in a very comprehensive manner. <br> <br> I find this area of study
+                  creatively fulfilling, with projects that require an eye for detail and an
+                  aptitude for problem solving. I enjoy the process of planning a website
+                  from a simple blueprint and then using a combination of HTML, CSS and
+                  JavaScript to complete the site. <br> <br> Over the past six months, I have improved my
+                   understanding of front-end development and have recently
+                   started to use PHP for server-side protocols. I am continuing to improve my
+                   understanding in these areas and also intend to learn new languages such C# and Java,
+                    as I have a keen interest in mobile-app development. <br> <br> I am confident in my
+                    current understanding of both front-end and back-end development and look forward
+                    to applying these skills within a commercial environment, in the near future.
+
+                  </p>
+                </div>
+                <div class="additional_wrapper">
+
+                <img class="profile_image" src="../Images/profile.jpg" alt="William Sears" width="267" height="369">
+                <div class="skills">
+                  <h3>Skills</h3>
+                  <ul>
+                    <li>HTML/CSS/SASS</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>SQL</li>
+                  </ul>
+                </div>
+
+                <div class="achievements">
+                  <h3>Achievements</h3>
+                  <ul>
+                    <li>Graduated from Oxford Brookes University with First Class Honours in Music.</li>
+                    <li class="second_achievement">Composed and recorded soundtracks for Electric Turtle Games and Wildseed Studios.</li>
+                  </ul>
+                </div>
+
+                </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </main>
+
+  <footer>
+    <div class="inner">
+      <div class="container">
+        <p class="message">Thank you for stopping by <i class="far fa-smile"></i></p>
+        <a id="about_up" href="#">
+          <p class="up"><i class="fas fa-chevron-up"></i>
+            Back to the top<br></p>
+        </a>
+      </div>
+    </div>
+  </footer>
+  <script src="../dist/core.js"></script>
+  <script src="../js/about.js"></script>
+</body>
+
+</html>
---
+++ templates/scs-scheme.php
@@ -0,0 +1,86 @@
+<?php include 'includes/head.php' ?>
+
+<?php include 'includes/topnav.php' ?>
+
+<?php include 'includes/sidenav.php' ?>
+
+  <!-- main content -->
+  <main>
+
+    <div id="page">
+      <div class="inner">
+        <div class="layer">
+          <div class="alignment">
+            <div id="scs_arrow_align">
+              <div id="close"><i id="scs_close" class="fas fa-arrow-circle-up"></i></div>
+              <div id="reveal"><i id="scs_reveal" class="fas fa-arrow-circle-down"></i></div>
+            </div>
+
+
+            <div class="all_text">
+              <div class="first_wrapper">
+                <div class="introduction">
+                  <h1 class="scheme_heading">Introduction to Scion Coalition Scheme</h1>
+                  <p class="intro_text"> The Scion Coalition Scheme is an intensive, specially tailored training program
+                    run by Netmatters in order to give willing candidates the opportunity to enter
+                    the industry as web developers. Under the supervision of senior web developers,
+                    scions generally aim to complete training within six to nine months.
+                    The course is intensive and therefore the level of learning achieved
+                    is extensive in a short space of time.</p>
+                </div>
+              </div>
+
+              <div class="second_wrapper">
+                <div class="treehouse">
+                  <div class="tree_heading">
+                    <h2>Treehouse
+
+                    </h2>
+                  </div>
+                  <p class="treehouse_text">Treehouse is an online learning community, featuring videos covering
+                    a number of topics from basic HTML to C# programming, iOS development,
+                    data analysis, and more. By completing courses users can earn points,
+                    allowing them to track their progress and see how much they have covered
+                    in certain areas.
+                    <span class="space"></span>
+                    Total Score: 17,782 <br>
+                    <a class="profile_link" href="https://teamtreehouse.com/williamsears2">teamtreehouse.com/williamsears2</a>
+                  </p>
+                </div>
+                <div class="netmatters">
+                    <h3 class="net_heading">About Netmatters</h3>
+                    <ul class="net_list">
+                    <li class="net_item">Established in 2008</li>
+                    <li class="net_item">Norfolk's leading technology company</li>
+                    <li class="net_item">Winner of the Princess Royal Training Award</li>
+                    <li class="net_item">Winner of EDP Skills of Tomorrow Award</li>
+                    <li class="net_item">80+ staff, 2 locations across Norfolk</li>
+                    <li class="net_item">Digital Marketing, Website & <br>Software development & IT Support</li>
+                    <li class="net_item">Broad spectrum of clients, working nationwide</li>
+                    <li class="net_item">Operate to strict company values</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main>
+
+  <footer class="scs_foot">
+    <div class="inner">
+      <div class="container">
+        <a id="up" href="#">
+          <p class="up"><i class="fas fa-chevron-up"></i>
+            Back to the top<br></p>
+        </a>
+      </div>
+    </div>
+  </footer>
+  <script src="../dist/core.js"></script>
+  <script src="../js/scs.js"></script>
+</body>
+
+</html>
