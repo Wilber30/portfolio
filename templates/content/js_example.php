5e95538b586593795e2ad7539a67878729279288
Wilber30 <wilbersears@gmail.com>
2023-02-03 11:16:59 +0000

    Adds content files

---
+++ templates/content/js_example.php
@@ -0,0 +1,34 @@
+{// Form Validation & localStorage check
+  form.addEventListener('submit', (e) => {
+  let messages = []
+
+  if (email.value === '' || email.value == null) {
+    messages.push('Please enter an email address')
+  } else if (!isEmail(email.value)) {
+    messages.push('Invalid email address')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    error.innerText = messages.join(', ')
+    return;
+    } else if (isEmail(email.value)) {
+  }
+
+  if (localStorage.getItem(email.value) !== null) {
+    e.preventDefault();
+    addImgToRow();
+  }
+
+  else {
+    e.preventDefault();
+    addEmailRow();
+    localStorage.setItem(email.value, email.value);
+  }
+  })
+
+  // Function tests variable against regex format
+  function isEmail(i) {
+  return /^([a-z\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]
+  {2,8})?$/.test(i);
+  }}
---
+++ templates/content/sql_query.php
@@ -0,0 +1,17 @@
+SELECT
+  m.mov_id AS "ID",
+  mov_title AS "Movie",
+  r.rev_stars AS "Star Rating"
+FROM
+  movie AS m
+  INNER JOIN (
+    SELECT
+      mov_id,
+      rev_stars
+    FROM
+      rating
+    WHERE
+      rev_stars > 3
+  ) AS r ON m.mov_id = r.mov_id
+ORDER BY
+  r.rev_stars ASC;
