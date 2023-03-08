8697b0d5bc4b541d64e31094bede091ebab67640
Wilber30 <wilbersears@gmail.com>
2023-03-08 13:39:55 +0000

    Comments-out cards

--- index.php
+++ index.php
@@ -47,91 +47,93 @@
         <h1 id="port">Portfolio</h1>
 
         <div class="card_wrapper top_row">
+            <h2>Coming Soon!</h2>
 
-            <div class="card">
-              <div class="img_container">
-                <a href="https://arrayreflection.william-sears.netmatters-scs.co.uk/" target="_blank">
-                  <img class="project" src="src/assets/images/arrayreflection.png" alt="Array Reflection">
-                </a>
-              </div>
-                <div class="card_text">
-                  <h3 class="project_title">Array Reflection</h3>
-                    <p class="project_description">A JavaScript project that assigns <br> an email address to an image loaded from UnSplash API.</p>
-                  <form >
-                    <button class="project_link array_btn" formaction="https://arrayreflection.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-                  </form>
-                </div>
-              </div>
+          <!-- <div class="card">
+            <div class="img_container">
+              <a href="https://arrayreflection.william-sears.netmatters-scs.co.uk/" target="_blank">
+                <img class="project" src="src/assets/images/arrayreflection.png" alt="Array Reflection">
+              </a>
+            </div>
+            <div class="card_text">
+              <h3 class="project_title">Array Reflection</h3>
+                <p class="project_description">A JavaScript project that assigns <br> an email address to an image loaded from UnSplash API.</p>
+              <form >
+                <button class="project_link array_btn" formaction="https://arrayreflection.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
+              </form>
+            </div>
+          </div>
 
-                <div class="card">
-                  <div class="img_container">
-                    <a href="https://netmatters.william-sears.netmatters-scs.co.uk/" target="_blank">
-                      <img class="project" src="src/assets/images/Placeholder-1.png" alt="Netmatters site">
-                    </a>
-                  </div>
-                    <div class="card_text">
-                        <h3 class="project_title">Netmatters Homepage</h3>
-                          <p class="project_description">A reconstruction of the Netmatters homepage and contact page.</p>
-                      <form >
-                        <button class="project_link net_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-                      </form>
-                    </div>
-                  </div>
+          <div class="card">
+            <div class="img_container">
+              <a href="https://netmatters.william-sears.netmatters-scs.co.uk/" target="_blank">
+                <img class="project" src="src/assets/images/Placeholder-1.png" alt="Netmatters site">
+              </a>
+            </div>
+            <div class="card_text">
+                <h3 class="project_title">Netmatters Homepage</h3>
+                  <p class="project_description">A reconstruction of the Netmatters homepage and contact page.</p>
+              <form >
+                <button class="project_link net_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
+              </form>
+            </div>
+          </div>
 
-                  <div class="card">
-                    <div class="img_container">
-                      <a href="https://wordpress.william-sears.netmatters-scs.co.uk/" target="_blank">
-                        <img class="project" src="src/assets/images/Placeholder-4.png" alt="Wordpress reflection task">
-                      </a>
-                    </div>
-                      <div class="card_text">
-                          <h3 class="project_title">Wordpress Reflection</h3>
-                            <p class="project_description axure">A site built using Wordpress themes and plugin architecture.</p>
-                        <form >
-                          <button class="project_link word_btn" formaction="https://wordpress.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-                        </form>
-                      </div>
-                    </div>
+          <div class="card">
+            <div class="img_container">
+              <a href="https://wordpress.william-sears.netmatters-scs.co.uk/" target="_blank">
+                <img class="project" src="src/assets/images/Placeholder-4.png" alt="Wordpress reflection task">
+              </a>
+            </div>
+            <div class="card_text">
+                <h3 class="project_title">Wordpress Reflection</h3>
+                  <p class="project_description axure">A site built using Wordpress themes and plugin architecture.</p>
+              <form >
+                <button class="project_link word_btn" formaction="https://wordpress.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
+              </form>
+            </div>
+          </div>
 
-                </div>
+        </div>
 
-                <div class="card_wrapper bottom_row">
-                  <div class="card">
-                    <div class="img_container">
-                      <a href="https://3ny7nh.axshare.com/#id=w7l75t" target="_blank">
-                        <img class="project" src="src/assets/images/Placeholder-3.png" alt="Axure shopping app">
-                      </a>
-                    </div>
-                      <div class="card_text">
-                          <h3 class="project_title">Axure Shopping App</h3>
-                            <p class="project_description axure">A prototype for a shopping cart application, which I designed using Axure.</p>
-                        <form >
-                          <button class="project_link axure_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-                        </form>
-                      </div>
-                    </div>
+        <div class="card_wrapper bottom_row">
 
-                    <div class="card">
-                      <div class="img_container">
-                        <a href="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" target="_blank">
-                          <img class="project" src="src/assets/images/Placeholder-2.png" alt="Quality builders collaborative task">
-                        </a>
-                      </div>
-                        <div class="card_text">
-                            <h3 class="project_title">Collaborative Project</h3>
-                              <p class="project_description axure">I constructed this site in collaboration with another student on the SCS course.</p>
-                          <form >
-                            <button class="project_link" formaction="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-                          </form>
-                        </div>
-                      </div>
+          <div class="card">
+            <div class="img_container">
+              <a href="https://3ny7nh.axshare.com/#id=w7l75t" target="_blank">
+                <img class="project" src="src/assets/images/Placeholder-3.png" alt="Axure shopping app">
+              </a>
+            </div>
+            <div class="card_text">
+                <h3 class="project_title">Axure Shopping App</h3>
+                  <p class="project_description axure">A prototype for a shopping cart application, which I designed using Axure.</p>
+              <form >
+                <button class="project_link axure_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
+              </form>
+            </div>
+          </div>
 
-                    </div>
+          <div class="card">
+            <div class="img_container">
+              <a href="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" target="_blank">
+                <img class="project" src="src/assets/images/Placeholder-2.png" alt="Quality builders collaborative task">
+              </a>
+            </div>
+            <div class="card_text">
+              <h3 class="project_title">Collaborative Project</h3>
+                  <p class="project_description axure">I constructed this site in collaboration with another student on the SCS course.</p>
+              <form >
+                <button class="project_link" formaction="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
+              </form>
+            </div>
+          </div>
 
+        </div> -->
 
-        <div class="blank_space">&nbsp;</div>
+        <!-- <div class="blank_space">&nbsp;</div> -->
 
       </div>
+    </div>
     </div>
 
     <!-- Contact -->
