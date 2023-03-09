d9148d8303db08f8759843aa19b9bc869b1d192a
Wilber30 <wilbersears@gmail.com>
2023-03-09 16:14:26 +0000

    Updates email section and removes cards

--- index.php
+++ index.php
@@ -48,92 +48,8 @@
 
         <div class="card_wrapper top_row">
             <h2>Coming Soon!</h2>
-
-          <!-- <div class="card">
-            <div class="img_container">
-              <a href="https://arrayreflection.william-sears.netmatters-scs.co.uk/" target="_blank">
-                <img class="project" src="src/assets/images/arrayreflection.png" alt="Array Reflection">
-              </a>
-            </div>
-            <div class="card_text">
-              <h3 class="project_title">Array Reflection</h3>
-                <p class="project_description">A JavaScript project that assigns <br> an email address to an image loaded from UnSplash API.</p>
-              <form >
-                <button class="project_link array_btn" formaction="https://arrayreflection.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-              </form>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="img_container">
-              <a href="https://netmatters.william-sears.netmatters-scs.co.uk/" target="_blank">
-                <img class="project" src="src/assets/images/Placeholder-1.png" alt="Netmatters site">
-              </a>
-            </div>
-            <div class="card_text">
-                <h3 class="project_title">Netmatters Homepage</h3>
-                  <p class="project_description">A reconstruction of the Netmatters homepage and contact page.</p>
-              <form >
-                <button class="project_link net_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-              </form>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="img_container">
-              <a href="https://wordpress.william-sears.netmatters-scs.co.uk/" target="_blank">
-                <img class="project" src="src/assets/images/Placeholder-4.png" alt="Wordpress reflection task">
-              </a>
-            </div>
-            <div class="card_text">
-                <h3 class="project_title">Wordpress Reflection</h3>
-                  <p class="project_description axure">A site built using Wordpress themes and plugin architecture.</p>
-              <form >
-                <button class="project_link word_btn" formaction="https://wordpress.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-              </form>
-            </div>
-          </div>
-
         </div>
-
-        <div class="card_wrapper bottom_row">
-
-          <div class="card">
-            <div class="img_container">
-              <a href="https://3ny7nh.axshare.com/#id=w7l75t" target="_blank">
-                <img class="project" src="src/assets/images/Placeholder-3.png" alt="Axure shopping app">
-              </a>
-            </div>
-            <div class="card_text">
-                <h3 class="project_title">Axure Shopping App</h3>
-                  <p class="project_description axure">A prototype for a shopping cart application, which I designed using Axure.</p>
-              <form >
-                <button class="project_link axure_btn" formaction="https://netmatters.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-              </form>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="img_container">
-              <a href="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" target="_blank">
-                <img class="project" src="src/assets/images/Placeholder-2.png" alt="Quality builders collaborative task">
-              </a>
-            </div>
-            <div class="card_text">
-              <h3 class="project_title">Collaborative Project</h3>
-                  <p class="project_description axure">I constructed this site in collaboration with another student on the SCS course.</p>
-              <form >
-                <button class="project_link" formaction="http://qualitybuilders.william-sears.netmatters-scs.co.uk/" formtarget="_blank">View Project</button>
-              </form>
-            </div>
-          </div>
-
-        </div> -->
-
-        <!-- <div class="blank_space">&nbsp;</div> -->
-
       </div>
-    </div>
     </div>
 
     <!-- Contact -->
@@ -143,7 +59,9 @@
 
           <div class="sm_contact_text">
             <h2>Get In Touch</h2>
-            <p><b>Email address:</b> william.sears@netmatters-scs.com</p>
+            <p>Email address: 
+              <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
+            </p>
             <p class="contact_detail">You can also contact me via the above email address.</p>
           </div>
 
@@ -153,7 +71,7 @@
               <h2 class="contact_header">Get In Touch</h2>
               <p class="info">Please feel free to get in touch with me via the contact form.</p>
               <h3 class="email_header">Email address:</h3>
-              <a class="email_address" href="mailto:william.sears@netmatters-scs.com">william.sears@netmatters-scs.com</a>
+              <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
               <p class="contact_detail">You can also contact me via the above email address.</p>
             </div>
 
