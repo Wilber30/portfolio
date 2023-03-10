62aca8b221164d937c41cd1a141a12f0b0f7cfcf
Wilber30 <wilbersears@gmail.com>
2023-03-10 12:11:18 +0000

    Updates indentation

--- index.php
+++ index.php
@@ -7,38 +7,32 @@
   <!-- Page content -->
   <main>
     <!-- <div id="full-page"> -->
+    <div id="banner">
+      <div class="type-container">
+        <div id="arrow_alignment">
+          <div id="close"><i class="fas fa-arrow-circle-up home_close"></i></div>
+          <div id="reveal"><i class="fas fa-arrow-circle-down home_reveal"></i></div>
+        </div>
 
-      <div id="banner">
+        <div class="typewriter">
+          <h1 id="app"></h1>
+        </div>
 
-        <div class="type-container">
+        <div class="fade-in">
+          <p class="me">I am a Web Developer</p>
+        </div>
 
-          <div id="arrow_alignment">
-            <div id="close"><i class="fas fa-arrow-circle-up home_close"></i></div>
-            <div id="reveal"><i class="fas fa-arrow-circle-down home_reveal"></i></div>
+        <div class="align_scroll">
+          <div class="scroll-down">
+            <a href="#port">
+              <p id="scroll">Scroll Down<br>
+                <i class="fas fa-chevron-down"></i>
+              </p>
+            </a>
           </div>
-
-          <div class="typewriter">
-            <h1 id="app"></h1>
-          </div>
-          <div class="fade-in">
-            <p class="me">I am a Web Developer</p>
-          </div>
-
-          <div class="align_scroll">
-
-            <div class="scroll-down">
-              <a href="#port">
-                <p id="scroll">Scroll Down<br>
-                  <i class="fas fa-chevron-down"></i>
-                </p>
-              </a>
-            </div>
-
-          </div>
-
-
         </div>
       </div>
+    </div>
 
     <!-- Project cards -->
     <div class="inner port_section">
@@ -56,48 +50,55 @@
     <div class="contact_section">
       <div class="contact_inner">
         <div class="container contact_container">
-
           <div class="sm_contact_text">
             <h2>Get In Touch</h2>
+
             <p>Email address: 
               <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
             </p>
+
             <p class="contact_detail">You can also contact me via the above email address.</p>
           </div>
 
           <div id="contact" class="contact_wrapper">
-
             <div class="contact_text">
               <h2 class="contact_header">Get In Touch</h2>
+
               <p class="info">Please feel free to get in touch with me via the contact form.</p>
+
               <h3 class="email_header">Email address:</h3>
+
               <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
+
               <p class="contact_detail">You can also contact me via the above email address.</p>
             </div>
 
             <div id="form">
               <div id="response"></div>
+
               <div id="error"></div>
+
               <div class="user">
                 <input id="name" type="text" name="name" placeholder="Name:" required="required">
+
                 <input id="email" name="email" type="text" placeholder="Email:" required="required">
               </div>
               <input id="cc" class="input" name="cc" placeholder="Cc:">
+
               <input id="subject" class="input" type="text" name="subject" placeholder="Subject" required="required">
+
               <textarea id="message" name="message" rows="8" cols="80" placeholder="Message:" required></textarea>
+
               <div class="btn_container">
                  <button id="sub_btn" type="submit" name="Submit">Submit</button>
               </div>
             </div>
-
           </div> <!-- contact_wrapper end -->
-
         </div>
       </div>
     </div>
   </main>
   <footer>
-
     <div class="inner">
       <div class="container">
         <a id="up" href="#">
@@ -106,10 +107,8 @@
         </a>
       </div>
     </div>
-
   </footer>
   <script src="src/js/dist/core.js"></script>
   <script src="main.js"></script>
 </body>
-
 </html>
