d9c7d1fa3de6e3be182bacbbe115217cde67e1da
Wilber30 <wilbersears@gmail.com>
2023-03-20 16:38:54 +0000

    Removes contact details/ adds php session

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
+<?php session_start(); ?>
 <?php include 'templates/layout/head.php' ?>
-
 <?php include 'templates/layout/topnav.php' ?>
-
 <?php include 'templates/layout/sidenav.php' ?>
 
   <!-- Page content -->
   <main>
     <!-- <div id="full-page"> -->
     <div id="banner">
+
       <div class="type-container">
+        <?php 
+          if(isset($_SESSION['status'])) 
+          {
+        ?>
+          <div id="alert_wrapper" class="alert_notice">
+            <div class="alert_text">
+              <strong>Thank you!</strong> <?php echo $_SESSION['status']; ?>
+
+              <i id="notice_close" class="fa-solid fa-xmark"></i>
+            </div>
+          </div>
+        <?php
+            unset($_SESSION['status']);
+          }
+        ?>
         <div id="arrow_alignment">
           <div id="close"><i class="fas fa-arrow-circle-up home_close"></i></div>
           <div id="reveal"><i class="fas fa-arrow-circle-down home_reveal"></i></div>
@@ -50,49 +65,35 @@
     <div class="contact_section">
       <div class="contact_inner">
         <div class="container contact_container">
-          <div class="sm_contact_text">
-            <h2>Get In Touch</h2>
-
-            <p>Email address: 
-              <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
-            </p>
-
-            <p class="contact_detail">You can also contact me via the above email address.</p>
-          </div>
-
           <div id="contact" class="contact_wrapper">
             <div class="contact_text">
               <h2 class="contact_header">Get In Touch</h2>
 
               <p class="info">Please feel free to get in touch with me via the contact form.</p>
-
-              <h3 class="email_header">Email address:</h3>
-
-              <a class="email_address" href="mailto:wilbersears@gmail.com">wilbersears@gmail.com</a>
-
-              <p class="contact_detail">You can also contact me via the above email address.</p>
             </div>
 
-            <div id="form">
-              <div id="response"></div>
+            <form id="contact_form" action="mailer.php" method="post">
+              <div id="form">
+                <div id="response"></div>
 
-              <div id="error"></div>
+                <div id="error"></div>
 
-              <div class="user">
-                <input id="name" type="text" name="name" placeholder="Name:" required="required">
+                <div class="user">
+                  <input id="name" type="text" name="name" placeholder="Name:" required="required">
 
-                <input id="email" name="email" type="text" placeholder="Email:" required="required">
+                  <input id="email" name="email" type="text" placeholder="Email:" required="required">
+                </div>
+                <input id="cc" class="input" name="cc" placeholder="Cc:">
+
+                <input id="subject" class="input" type="text" name="subject" placeholder="Subject" required="required">
+
+                <textarea id="message" name="message" rows="8" cols="80" placeholder="Message:" required></textarea>
+
+                <div class="btn_container">
+                  <button id="sub_btn" type="submit" name="Submit">Submit</button>
+                </div>
               </div>
-              <input id="cc" class="input" name="cc" placeholder="Cc:">
-
-              <input id="subject" class="input" type="text" name="subject" placeholder="Subject" required="required">
-
-              <textarea id="message" name="message" rows="8" cols="80" placeholder="Message:" required></textarea>
-
-              <div class="btn_container">
-                 <button id="sub_btn" type="submit" name="Submit">Submit</button>
-              </div>
-            </div>
+            </form>
           </div> <!-- contact_wrapper end -->
         </div>
       </div>
